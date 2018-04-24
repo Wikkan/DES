0210e41b66f343a9585e2be4aa1681c137ae7918
Adrián Garro <adriangarro81@gmail.com>
2018-04-24 12:23:45 -0600

    Small Changes

--- src/DES/TDESUI.scala
+++ src/DES/TDESUI.scala
@@ -31,44 +31,52 @@ class TDESUI extends Application {
   }
 
   override def start(primaryStage: Stage): Unit = {
+
     primaryStage.setTitle("Triple DES")
     val grid = new GridPane
     grid.setAlignment(Pos.CENTER)
     grid.setHgap(10)
     grid.setVgap(10)
     grid.setPadding(new Insets(25, 25, 25, 25))
+
     val sceneTitle = new Text("Bienvenid@")
     sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20))
     grid.add(sceneTitle, 0, 0, 2, 1)
+
     val mode = new Label("Modo: ")
     grid.add(mode, 0, 1)
     val modes = FXCollections.observableArrayList("Encriptar", "Desencriptar")
     val modesComboBox = new ComboBox[String](modes)
     modesComboBox.getSelectionModel.selectFirst()
     grid.add(modesComboBox, 1, 1)
+
     val key1Label = new Label("Llave 1:")
     grid.add(key1Label, 0, 2)
     val key1TextField = new TextField
     grid.add(key1TextField, 1, 2)
+
     val key2Label = new Label("Llave 2:")
     grid.add(key2Label, 0, 3)
     val key2TextField = new TextField
     grid.add(key2TextField, 1, 3)
+
     val key3Label = new Label("Llave 3:")
     grid.add(key3Label, 0, 4)
     val key3TextField = new TextField
     grid.add(key3TextField, 1, 4)
+
     val textLabel = new Label("Texto: ")
     grid.add(textLabel, 0, 5)
     val textField = new TextField
     grid.add(textField, 1, 5)
+
     val btn = new Button("Ejecutar TDES")
     val hbBtn = new HBox(10)
     hbBtn.setAlignment(Pos.BOTTOM_RIGHT)
     hbBtn.getChildren.add(btn)
     grid.add(hbBtn, 1, 6)
     btn.setOnAction((e: ActionEvent) => {
-      def foo(e: ActionEvent) = {
+      def foo(e: ActionEvent): Unit = {
         val anyFieldIncomplete = key1TextField.getText.trim.isEmpty || key2TextField.getText.trim.isEmpty || key3TextField.getText.trim.isEmpty || textField.getText.trim.isEmpty
         if (anyFieldIncomplete) showAlert("Advertencia", "¡Hay campos sin completar!")
         else if (modesComboBox.getValue == "Encriptar") {
@@ -88,8 +96,10 @@ class TDESUI extends Application {
       }
       foo(e)
     })
+
     val scene = new Scene(grid, 525, 300)
     primaryStage.setScene(scene)
     primaryStage.show()
+    
   }
 }
