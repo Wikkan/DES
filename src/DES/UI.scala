35fad377a3fbf58221ba7383e3420bba69776686
Adrián Garro <adriangarro81@gmail.com>
2018-05-02 09:56:25 -0600

    Avances en Interfaz

--- src/DES/UI.scala
+++ src/DES/UI.scala
@@ -4,7 +4,7 @@ import java.io.File
 import javafx.application.Application
 import javafx.geometry.Insets
 import javafx.geometry.Pos
-import javafx.scene.Scene
+import javafx.scene.{Group, Scene}
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
 import javafx.scene.text.Font
@@ -19,6 +19,11 @@ import javafx.scene.control.Alert.AlertType
 import javafx.event.ActionEvent
 import javafx.scene.layout.GridPane
 import javafx.geometry.HPos.LEFT
+import javafx.scene.control.TabPane
+import javafx.scene.control.TabPane.TabClosingPolicy
+import javafx.geometry.Pos
+import javafx.scene.layout.BorderPane
+import javafx.scene.control.Tab
 
 object UI {
   def main(args: Array[String]) {
@@ -41,19 +46,71 @@ class UI extends Application {
 
   override def start(primaryStage: Stage): Unit = {
 
-    primaryStage.setTitle("Triple DES")
-    val grid = new GridPane
-    grid.setAlignment(Pos.CENTER)
-    grid.setHgap(10)
-    grid.setVgap(10)
-    grid.setPadding(new Insets(25, 25, 25, 25))
-    grid.setStyle("-fx-background-color: #D3D3D3;")
+    primaryStage.setTitle("TRIPLE DES")
+    val root = new Group()
+    val scene = new Scene(root, 450, 300)
 
-    val sceneTitle = new Text("Bienvenid@")
-    sceneTitle.setFont(Font.font("Menlo", FontWeight.LIGHT, 16))
-    grid.add(sceneTitle, 0, 0, 2, 1)
+    val tabPane = new TabPane
+    tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE)
+    val lowerLabel = new Label("  Criptografía, TEC 2018  ")
 
-    val scene = new Scene(grid, 400, 300)
+    val mainBorderPane = new BorderPane
+    mainBorderPane.setTop(tabPane)
+    mainBorderPane.setBottom(lowerLabel)
+    BorderPane.setAlignment(lowerLabel, Pos.BOTTOM_RIGHT)
+    mainBorderPane.prefHeightProperty.bind(scene.heightProperty)
+    mainBorderPane.prefWidthProperty.bind(scene.widthProperty)
+
+    /* ENCRYPTION TAB */
+
+    val encryptionTab = new Tab
+    encryptionTab.setText("Encriptar")
+
+    val encryptionGrid = new GridPane
+    encryptionGrid.setAlignment(Pos.CENTER)
+    encryptionGrid.setHgap(10)
+    encryptionGrid.setVgap(10)
+    encryptionGrid.setPadding(new Insets(25, 25, 25, 25))
+
+    val encKeysType = new Label("Tipo de llaves: ")
+    encryptionGrid.add(encKeysType, 0, 0)
+    val encKeysTypeColl = FXCollections.observableArrayList("Texto", "Hexadecimal")
+    val encKeysTypeComboBox = new ComboBox[String](encKeysTypeColl)
+    encKeysTypeComboBox.getSelectionModel().select(0)
+    encryptionGrid.add(encKeysTypeComboBox, 1, 0)
+
+    val encKey1Label = new Label("Llave 1:")
+    encryptionGrid.add(encKey1Label, 0, 1)
+    val encKey1TextField = new TextField
+    encryptionGrid.add(encKey1TextField, 1, 1)
+
+    val encKey2Label = new Label("Llave 2:")
+    encryptionGrid.add(encKey2Label, 0, 2)
+    val encKey2TextField = new TextField
+    encryptionGrid.add(encKey2TextField, 1, 2)
+
+    val encKey3Label = new Label("Llave 3:")
+    encryptionGrid.add(encKey3Label, 0, 3)
+    val encKey3TextField = new TextField
+    encryptionGrid.add(encKey3TextField, 1, 3)
+
+    val encInputType = new Label("Tipo de entrada: ")
+    encryptionGrid.add(encInputType, 0, 4)
+    val encInputTypeColl = FXCollections.observableArrayList("Texto", "Hexadecimal", "Archivo")
+    val encInputTypeComboBox = new ComboBox[String](encInputTypeColl)
+    encInputTypeComboBox.getSelectionModel().select(0)
+    encryptionGrid.add(encInputTypeComboBox, 1, 4)
+
+    encryptionTab.setContent(encryptionGrid)
+    tabPane.getTabs.add(encryptionTab)
+
+    /* DECRYPTION TAB */
+
+    val decryptionTab = new Tab
+    decryptionTab.setText("Decriptar")
+    tabPane.getTabs.add(decryptionTab)
+
+    root.getChildren.add(mainBorderPane)
     primaryStage.setScene(scene)
     primaryStage.show()
 
